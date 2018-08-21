6f63a8ae5eca32b2f4e48a08b4b180b233efd44c
Nicolas Bertin <nicolas.bertin0@gmail.com>
2018-08-21 15:05:51 +0200

    carousel working wouallez

--- app/assets/javascripts/components/carousel.js
+++ app/assets/javascripts/components/carousel.js
@@ -1,10 +1,59 @@
+var cursor = 2,
+    slideWidth = 802;
+
 $(document).ready(function() {
 
-  $(".next").click(function(){
-      $(".food-pics").animate({left: '-=800px'});
+
+
+  $(".next").click(function() {
+    next()
   });
 
-  $(".previous").click(function(){
-      $(".food-pics").animate({left: '+=800px'});
+  $(".previous").click(function() {
+    previous()
+  });
+
+  $(".image-dot").click(function() {
+
+
+
+    let shift = $(this).val() - cursor;
+    let shiftpx = {
+      'left': -$(this).val() * slideWidth + 950
+    };
+    console.log(cursor, shift)
+    $(".food-pics").animate(shiftpx);
+    cursor = $(this).val()
+    console.log($(this).val())
+    console.log(cursor, shift)
+  });
+
+  $(".image-dot, .previous, .next").click(function() {
+    $(".image-dot").removeClass("active-dot");
+    $('[value="' + cursor + '"]').addClass("active-dot");
+    console.log($('.image-dot option[value="' + cursor + '"]'));
   });
 })
+
+
+function next() {
+  if (cursor === 5) {
+    $(".food-pics").animate({left: '+=3200px'});
+    cursor = 1;
+  } else {
+    $(".food-pics").animate({left: '-=800px'});
+    cursor++;
+  }
+  console.log(cursor)
+}
+
+function previous() {
+  if (cursor === 1) {
+    $(".food-pics").animate({left: '-=3200px'});
+    cursor = 5;
+  } else {
+    $(".food-pics").animate({left: '+=800px'});
+    cursor -= 1;
+  }
+  console.log(cursor)
+}
--- app/assets/javascripts/components/tabs.js
+++ app/assets/javascripts/components/tabs.js
@@ -6,24 +6,24 @@ $(document).ready(function() {
 
 
   $(".presentation-btn").click(function() {
-    $(".active-tab").removeClass("active-tab").hide(300, function() {
-      $(".presentation-tab").addClass("active-tab").show(300);
+    $(".active-tab").removeClass("active-tab").fadeOut(300, function() {
+      $(".presentation-tab").addClass("active-tab").fadeIn(300);
     });
     $(".tab-btn").prop('disabled', false);
     $(".presentation-btn").prop('disabled', true);
   });
 
   $(".menu-btn").click(function() {
-    $(".active-tab").removeClass("active-tab").hide(300, function() {
-      $(".menu-tab").addClass("active-tab").show(300);
+    $(".active-tab").removeClass("active-tab").fadeOut(300, function() {
+      $(".menu-tab").addClass("active-tab").fadeIn(300);
     });
     $(".tab-btn").prop('disabled', false);
     $(".menu-btn").prop('disabled', true);
   });
 
   $(".reference-btn").click(function() {
-    $(".active-tab").removeClass("active-tab").hide(300, function() {
-      $(".reference-tab").addClass("active-tab").show(300);
+    $(".active-tab").removeClass("active-tab").fadeOut(300, function() {
+      $(".reference-tab").addClass("active-tab").fadeIn(300);
     });
     $(".tab-btn").prop('disabled', false);
     $(".reference-btn").prop('disabled', true);
